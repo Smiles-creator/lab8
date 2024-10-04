62b2e495da8f30977a86d4e007f625607a234777
Булавинова София <bulavinova07s@gmail.com>
2024-10-04 13:11:22 +0300

    Изменение первого файла

--- hello.pas
+++ hello.pas
@@ -1,3 +1,9 @@
 ﻿begin
-  Println($'Привет, мир!')
-end.
+  var time := ReadInteger('Введите час');
+  Assert(time in 0..23);
+  case hout of
+    4..10: Println('Доброе утро, Мир!');
+    11..16: Println('Добрый день, Мир!');
+    17..22: Println('Добрый вечер, Мир!');
+    else Println('Доброй ночи, Мир!');
+  end;
