c3e0661fcfb5001975e4b2dbac17d7ebfdec73cc
Булавинова София <bulavinova07s@gmail.com>
2024-10-04 13:22:02 +0300

    Добавлен файл на проверку года на високосность

---
+++ time.pas
@@ -0,0 +1,10 @@
+﻿begin
+  var time := ReadInteger('Введите год: ');
+  if (time mod 4 = 0) and not(time mod 100 = 0) and (time mod 400 <> 0) then
+    begin
+  Println('Год високосный');
+  end
+  else begin
+    Println('Год не високосный');
+  end;
+end.
